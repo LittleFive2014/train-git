b50b484c40e2fae55f272f15e952152d597f0083
n.hai <n.hai@wesoft.co.jp>
2024-06-19 14:09:33 +0900

    add comment

--- api/openapi/src/main/java/jp/co/wesoft/autocalc/OpenApiApplication.java
+++ api/openapi/src/main/java/jp/co/wesoft/autocalc/OpenApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenApiApplication {
 
     public static void main(String[] args) {
+		//spring start
         SpringApplication.run(OpenApiApplication.class, args);
     }
 
