ded7b8125583a8e65f93c1ee095ba684472054ec
n.hai <n.hai@wesoft.co.jp>
2024-06-19 15:49:54 +0900

    test

--- api/openapi/src/main/java/jp/co/wesoft/autocalc/OpenApiApplication.java
+++ api/openapi/src/main/java/jp/co/wesoft/autocalc/OpenApiApplication.java
@@ -12,11 +12,6 @@ public class OpenApiApplication {
 		String param2 =　"abc";
 		String param3 =　"abc";
 		String param4 =　"abc";
-		String param5 =　"abc";
-		String param6 =　"abc";
-		String param7 =　"abc";
-		String param8 =　"abc";
-		String param9 =　"abc";
         SpringApplication.run(OpenApiApplication.class, args);
     }
 
