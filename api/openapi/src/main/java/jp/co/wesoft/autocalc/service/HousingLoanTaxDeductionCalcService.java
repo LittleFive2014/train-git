d8e54be5f0b2a050cd9a36a763e0c91ba2003f0d
n.hai <n.hai@wesoft.co.jp>
2024-06-20 10:10:54 +0900

    rebase commit 1

--- api/openapi/src/main/java/jp/co/wesoft/autocalc/service/HousingLoanTaxDeductionCalcService.java
+++ api/openapi/src/main/java/jp/co/wesoft/autocalc/service/HousingLoanTaxDeductionCalcService.java
@@ -4,6 +4,7 @@ import jp.co.wesoft.autocalc.web.vo.HousingLoanTaxDeductionShowItemRequest;
 import jp.co.wesoft.autocalc.web.vo.HousingLoanTaxDeductionCalcRequest.HousingLoanTaxDeduction;
 
 
+
 public interface HousingLoanTaxDeductionCalcService {
     Integer autoCalc(HousingLoanTaxDeduction housingLoanTaxDeduction, int adjustmentYear);
    HousingLoanTaxDeductionShowItemRequest.HousingLoanTaxShowItem showItem(HousingLoanTaxDeduction housingLoanTaxDeduction, int adjustmentYear);
